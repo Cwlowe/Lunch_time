8cffe99455cc127ed8ace664824aaa6293463b20
Leslie Meng <mengli26@gmail.com>
2021-04-21 12:18:29 -0400

    started salary form

---
+++ pages/Salary.js
@@ -0,0 +1,11 @@
+export default function Salary() {
+	return (
+		<section>
+			<form>
+				<input type="number" placeholder="paycheck amount"></input>
+				<input type="date" placeholder="last paid"></input>
+				<input type="number" placeholder="weeks between each paycheck"></input>
+			</form>
+		</section>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,56 @@
-import Head from 'next/head'
-import HelloWorld from '../components/HelloWorld'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import HelloWorld from '../components/HelloWorld';
+import styles from '../styles/Home.module.css';
+import Salary from './Salary';
+import { useState } from 'react';
 
 export default function Home() {
-  const transactions = HelloWorld() || []
+	const transactions = HelloWorld() || [];
+	const [state, setState] = useState({ funds: 0, lastPaid: '', cadence: 0 });
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					Welcome to <a href="https://nextjs.org">Next.js!</a>
+				</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+				<p className={styles.description}>
+					Get started by editing{' '}
+					<code className={styles.code}>pages/index.js</code>
+				</p>
+				<Salary
+					adjustSalary={(newSal, date, cadence) =>
+						setState({ funds: newSal, lastPaid: date, cadence: cadence })
+					}
+				/>
+				<div className={styles.code}>
+					{transactions.map((item) => {
+						return (
+							<div key={item.ID__A || 0}>
+								<p>{item.Purchase__B}</p>
+								<p>{item.Price__C}</p>
+							</div>
+						);
+						// <p key={item.ID__A || 0}>{item.Purchase__B}</p>
+					})}
+				</div>
+			</main>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-        
-        <div className={styles.code}>
-          {transactions.map((item)=>{
-          return<p key={item.ID__A || 0}>{item.Purchase__B}</p>
-        })}
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+			<footer className={styles.footer}>
+				<a
+					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Powered by{' '}
+					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+				</a>
+			</footer>
+		</div>
+	);
 }
