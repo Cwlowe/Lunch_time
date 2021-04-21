a2c08d18c997da9870d829bd3511ba7d5611b9d1
Leslie Meng <mengli26@gmail.com>
2021-04-21 12:18:53 -0400

    started salary form

--- pages/Salary.js
+++ pages/Salary.js
@@ -1,7 +1,13 @@
-export default function Salary() {
+import { setState } from 'react';
+export default function Salary(props) {
 	return (
 		<section>
-			<form>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					adjustSalary();
+				}}
+			>
 				<input type="number" placeholder="paycheck amount"></input>
 				<input type="date" placeholder="last paid"></input>
 				<input type="number" placeholder="weeks between each paycheck"></input>
