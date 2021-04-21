b76263f94a94a6bdd4e8510b3f0a790ecb86c9d0
Cwlowe <calvinwolfelowe@gmail.com>
2021-04-21 14:15:09 -0700

    STYLED: front page, moved components out of the page folder

--- components/AffordLunch.js
+++ components/AffordLunch.js
@@ -2,7 +2,6 @@ export default function AffordLunch(props) {
 	let total = props.amount;
 	let nextPayDate = new Date();
 	//  + 7 * props.cadence).slice(0, 10);
-	console.log(nextPayDate);
 	for (let i = 0; i < props.expenses.length; i++) {
 		if (props.expenses.Date__E < nextPayDate) total += props.expenses.Price__C;
 	}
