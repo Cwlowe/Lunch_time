68ecefbbd91910d744a71f146b1319bce1d88d7b
Cwlowe <calvinwolfelowe@gmail.com>
2021-04-22 08:28:58 -0700

    ADDED: Styles to the image

--- components/AffordLunch.js
+++ components/AffordLunch.js
@@ -1,6 +1,7 @@
 import { Grid } from '@material-ui/core';
 import styles from '../styles/AffordLunch.module.css';
 import Expense from './Expenses';
+import Image from 'next/image'
 
 export default function AffordLunch(props) {
 	let total = parseInt(props.curr.funds);
@@ -12,30 +13,47 @@ export default function AffordLunch(props) {
 	let randomFood = afford.length
 		? afford[Math.floor(Math.random() * afford.length)]
 		: null;
-	console.log(foods, afford, randomFood, 'hi');
 	for (let i = 0; i < props.expenses.length; i++) {
 		total -= props.expenses[i].Price__C;
 	}
 
 	return (
 		<Grid container justify="center" alignItems="center">
-			<Grid item xs={5}>
+			<Grid item xs={6}>
 				<h2>Can I afford lunch today?</h2>
 				<p className={styles.total}>{total > 0 ? 'Yes!' : 'No'}</p>
 			</Grid>
-			<Grid item xs={5}>
+			<Grid item xs={6}>
 				<h2>Remaining funds before next paycheck?</h2>
 				<p className={styles.total}>
 					${Math.round(100 * parseFloat(total)) / 100}
 				</p>
+				
+			</Grid>
+			<Grid item xs={12}>
+				<Grid container justify="center" alignItems="center">
 				{randomFood ? (
 					<>
-						<p className={styles.total}>{`That's ${Math.floor(
-							total / randomFood.Price__C
-						)} ${randomFood.Food__A}s. ${randomFood.Desc__B}`}</p>
-						<img src={`${randomFood.Url__D}`} />{' '}
+						<Grid item xs={12}>
+						<h2 className={styles.total}>{`That's ${Math.floor(
+								total / randomFood.Price__C
+							)} ${randomFood.Food__A}s. ${randomFood.Desc__B}`}</h2>
+						</Grid>
+						
+
+						<Grid item xs={4}>
+						
+						<img
+							className={styles.image}
+							src={`${randomFood.Url__D}`} 
+							alt="Food Picture"
+							width={500}
+        					height={500}
+						/>
+						</Grid>
 					</>
 				) : null}
+				</Grid>
 			</Grid>
 		</Grid>
 	);
